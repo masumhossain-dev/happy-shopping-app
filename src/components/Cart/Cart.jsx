e0eb0e5c3586cf23148ebec843980cea33f09190
Masum Hossain <masumhossain.dev@gmail.com>
2023-08-19 00:03:11 +0600

    project redesigned

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+const Cart = ({ cart, handleClearCart, children }) => {
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+    const tax = totalPrice * 7 / 100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
+    return (
+        <div className='w-96 text-white text-2xl cart-color p-5 rounded-lg sticky top-8'>
+            <h4 className='text-center text-3xl font-bold mb-5'>Order Summary</h4>
+            <p className='mb-2.5'>Selected Items: {quantity}</p>
+            <p className='mb-2.5'>Total Price: ${totalPrice}</p>
+            <p className='mb-2.5'>Shipping: ${totalShipping}</p>
+            <p className='mb-2.5'>Tax: ${tax.toFixed(2)}</p>
+            <h6 className='font-bold'>Grand Total: ${grandTotal.toFixed(2)} </h6>
+            <button onClick={handleClearCart} className='btn btn-error text-white block mx-auto mt-10'>
+                <span>Clear Cart </span>
+                <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
+            {children}
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+import { Link } from 'react-router-dom';
+import img1 from '../../images/carousel/img-1.jpg';
+import img2 from '../../images/carousel/img-2.jpg';
+import img3 from '../../images/carousel/img-3.jpg';
+import img4 from '../../images/carousel/img-4.jpg';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products])
+
+    const handleAddToCart = (product) => {
+        let newCart = [];
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+        setCart(newCart);
+        addToDb(product.id)
+    }
+
+    const handleClearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
+    return (
+        <div>
+            <div className='flex justify-left'>
+                <div className="grid grid-cols-4 gap-16 mt-8 ms-8 me-12">
+                    {
+                        products.map(product => <Product
+                            key={product.id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                    }
+                </div>
+                <div className="mt-8">
+                    <Cart
+                        cart={cart}
+                        handleClearCart={handleClearCart}
+                    >
+                        <Link className='proceed-link' to="/orders">
+                            <button className='btn-proceed'>Review Order</button>
+                        </Link>
+                    </Cart>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
